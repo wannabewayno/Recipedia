b6d0f8a12a72087ed05f7b18c131354bfc5125a0
RJC Desktop <reuben.crocker@gmail.com>
2020-05-31 13:47:25 +0800

    changed var to const on server.js,

--- config/config.js
+++ config/config.js
@@ -1,9 +1,9 @@
-{
+module.exports = {
   "development": {
-    "username": "root",
-    "password": null,
-    "database": "recipedia_development",
-    "host": "127.0.0.1",
+    "username": process.env.MYSQL_USERNAME,
+    "password": process.env.MYSQL_PASSWORD,
+    "database": process.env.MYSQL_DATABASE,
+    "host": process.env.MYSQL_HOST,
     "dialect": "mysql",
     "operatorsAliases": false
   },
