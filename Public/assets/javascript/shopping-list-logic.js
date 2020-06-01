10226e35de0ad939b4932a131df3fcc8870c98e4
Wayne <wayne.c.griffiths@gmail.com>
2020-06-02 01:00:10 +0800

    shopping list BACK draft finnished

--- Public/assets/javascript/shopping-list-logic.js
+++ Public/assets/javascript/shopping-list-logic.js
@@ -1,5 +1,6 @@
 // TODO: FUNCTION:
 // scans shopping list to see if an entire recipe has been bought for and hides all those ingredients
+// will be triggered by crossing off an item
 
 // TODO: FUNCTION:
 // logic that shows what reicpes to buy for
@@ -7,3 +8,18 @@
 // retrieves all recipes you need to buy for.
 // if user manually adds a recipe this will be on the shopping list until it is bought for removed
 
+//TODO: FUNCTION:
+// manually adds a new item to the shopping list from the input at the top of the shopping list;
+// will have check if the user has disclosed units or not, 
+// probably need a consistency checker for solid or liquid to give everything in approproate units
+
+//TODO: FUNCTION:
+// when the menu button is pressed, flips the shopping-list over to reveal the options on the other side
+// needs a css flip
+// at half flip:
+//  hide front elements
+//  show back elements
+// changes icon to list icon 'to go back to list' will need two id's of buttons
+
+//TODO: FUNCTION 
+// any items on the list that have text-decoration:line-through will be removed from the list when the shopping list is opened again
