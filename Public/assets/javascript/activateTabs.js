5e18cf653597dda26e70414825c912448f0a1870
Wayne <wayne.c.griffiths@gmail.com>
2020-06-01 14:15:26 +0800

    UPDATE index.html, added multiple select options for cuisine and diet, removed unecessary grid divs

--- Public/assets/javascript/activateTabs.js
+++ Public/assets/javascript/activateTabs.js
@@ -1,8 +1,10 @@
-// activates Materialize JS for tabs and chips. Hides advanced search on start up.
+// activates Materialize JS for tabs, chips, input selects and dropdowns, Hides advanced search on start up.
 $(document).ready(function(){
-    $("#advanced-search").hide();
-    $('.tabs').tabs();
-    $('.chips').chips();
+  $("#advanced-search").hide();
+  $('.tabs').tabs();
+  $('.chips').chips();
+  $('select').formSelect();
+  $('.dropdown-trigger').dropdown();
 });
  
 //expands search bar when 'advanced' is clicked
