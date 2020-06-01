92f23c58334dffa55c243424a4fb5b41d21896e4
Wayne <wayne.c.griffiths@gmail.com>
2020-06-01 23:53:03 +0800

    shopping-list FRONT draft finalised

---
+++ Public/assets/javascript/shopping-list-logic.js
@@ -0,0 +1,9 @@
+// TODO: FUNCTION:
+// scans shopping list to see if an entire recipe has been bought for and hides all those ingredients
+
+// TODO: FUNCTION:
+// logic that shows what reicpes to buy for
+// this should interface with meal plan, we need a boolean status if a recipe has been bought for or not
+// retrieves all recipes you need to buy for.
+// if user manually adds a recipe this will be on the shopping list until it is bought for removed
+
