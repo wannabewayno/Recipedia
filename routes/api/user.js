c7e4b0608190205e1327748a04b2034c069ddf07
Wayne <wayne.c.griffiths@gmail.com>
2020-06-04 15:58:04 +0800

    added patch method at the user dashboard

--- routes/api/user.js
+++ routes/api/user.js
@@ -2,7 +2,16 @@
 const db = require("../../models");
 
 module.exports = app => {
-    app.post("/api/user", function(req, res) {
+
+    //! hey there team, this is a working patch request from the members.html
+    // I need someone to hook this up to our db
+    // this update's user information like first name, last name, email, diet and allergy stuff
+    // see the form in action at memeber.html
+
+    // the req.body will look like { field: <column to update>, value: <value to set to> }
+    // ? example: { field: first_name, value: wayne }
+    app.patch("/api/user", function(req, res) {
         console.log(req.body);
+        res.sendStatus(200);
     });
 }
