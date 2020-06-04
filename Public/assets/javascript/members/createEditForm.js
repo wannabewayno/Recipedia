d229f76a1479cbff6855aa5287b5b64bd9ebb72b
Wayne <wayne.c.griffiths@gmail.com>
2020-06-04 12:09:14 +0800

    added form to update user information

---
+++ Public/assets/javascript/members/createEditForm.js
@@ -0,0 +1,45 @@
+/**
+ * Creates the form below - send data via patch route to database to update individual user settings
+ * <form action="/api/user" method="patch">
+ *  <div class="input-field">
+ *   	<input id="editInput" value="data-action" ></input>
+ *   	<label for="editInput">data-label</label>
+ *   </div>
+ *   <button type="submit">
+ *       <i class="material-icons">publish</i>
+ *   </button>
+ * </form>
+ * @param {String} value - the item to edit 
+ */
+const createEditForm = (value,dataLabel) => {
+    // create all necessary html elements
+    const form = $('<form>').attr({'action':'/api/user','method':'patch'}).addClass('Valign-center');
+    const divInput = $('<div>').addClass('input-field');
+    const divButtons = $('<div>').addClass('Valign-center');
+    const label = $('<label>').attr('for','editInput').addClass('active').text(dataLabel);
+    const input = $('<input>').attr({'id':'editInput','value':value});
+    const buttonPublish = $('<button>').attr({'type':'submit'}).addClass('xgap-tiny')
+    const buttonUndo = $('<button>').attr({'type':'button'}).data({'value':value,'dataLabel':dataLabel}).addClass('xgap-tiny');
+    const iconPublish = $('<i>').addClass('material-icons').text('publish');
+    const iconUndo = $('<i>').addClass('material-icons').text('undo');
+
+    // arrange the elements
+    buttonPublish.append(iconPublish);
+    buttonUndo.append(iconUndo)
+    divInput.append(input,label);
+    divButtons.append(buttonPublish,buttonUndo)
+    form.append(divInput,divButtons);
+
+    return form;
+}
+
+// unction creates this mark up
+/* <form action="/api/user" method="patch">
+    <div class="input-field">
+    	<input id="editInput" value="data-action" ></input>
+    	<label for="editInput">data-label</label>
+    </div>
+    <button type="submit">
+        <i class="material-icons">publish</i>
+    </button>
+</form> */
--- Public/assets/javascript/members/index.js
+++ Public/assets/javascript/members/index.js
@@ -1,5 +1,6 @@
 // a list of all function paths this file depends on to run.
 const dependencies = [
+    `members/createEditForm.js`,
     //------ files not in this directory ------
     `getDropdownValues.js`
 ];
@@ -12,5 +13,27 @@ $(document).ready(function() {
     dependencies.forEach(dependency => {
         const script = `<script src="./assets/javascript/${dependency}"></script>`
         $(script).insertBefore('#index');
+    });
+
+    $('#account-settings, #user-preferences').hide();
+
+    // event listeners
+    //===========================================================================
+    $('#dropdown-preferences-btn').on('click',function(event){
+        $('#user-preferences').toggle("slow");
+    });
+
+    $('#dropdown-account-btn').on('click',function(event){
+        $('#account-settings').toggle("slow");
+    });
+
+    $('.edit-btn').on('click',function(event) {
+        const siblings = $(event.currentTarget).siblings()[0];
+        console.log(siblings);
+        const value = $(siblings).children('.data').text()
+        const dataLabel = $(siblings).children('.label').text()
+        const editForm = createEditForm(value,dataLabel);
+        const appendTarget = $(event.currentTarget).parents('li');
+        appendTarget.html(editForm);
     });
 })
