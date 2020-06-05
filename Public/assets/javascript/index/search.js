d087e9adbf1c14c6b115da9b1b03c916c8ade2c2
Wayne <wayne.c.griffiths@gmail.com>
2020-06-06 00:12:51 +0800

    fix localstorage

--- Public/assets/javascript/index/search.js
+++ Public/assets/javascript/index/search.js
@@ -15,12 +15,12 @@ const search = (query,type) => {
     .then( results => {
         //this comes from db
         console.log(results);
-        
+
         const { recipeCardInfo, recipeInfo } = results;
         // * now we can push our recipeInfo into local storage, so when we pull up the modal
         // * we don't have to do another query and have the user wait a few/fraction-of seconds
-        localstorage.setItem('searchResults',JSON.stringify(recipeInfo));
-
+        localStorage.setItem('searchResults',JSON.stringify(recipeInfo));
+        
         // * we now pass the recipeCardInfo to render the recipe cards for the user to look at
         // * when they click on any of these cards, it will pull that recipe from local storage
         renderResults(recipeCardInfo);
