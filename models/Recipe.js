f6f669506d81ef085f8d3467d2652590708bc1e8
RJC Desktop <reuben.crocker@gmail.com>
2020-06-05 22:50:45 +0800

    modified Recipe model and recipe routes

--- models/Recipe.js
+++ models/Recipe.js
@@ -7,6 +7,11 @@ module.exports = function(sequelize, DataTypes) {
         len: [1]
       }
     },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+
     ingredients: {
       type: DataTypes.JSON,
       allowNull: false,
@@ -31,7 +36,11 @@ module.exports = function(sequelize, DataTypes) {
       type: DataTypes.TEXT,
       allowNull: true
     },
-    cusine: {
+    prep_time: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    cuisine: {
       type: DataTypes.TEXT,
       allowNull: true
     },
@@ -41,7 +50,6 @@ module.exports = function(sequelize, DataTypes) {
     }
     
   }, {});
-
   
   Recipe.associate = function(models) {
     // We're saying that a Favourite should belong to a Recipe
