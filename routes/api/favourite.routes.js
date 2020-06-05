28e1487e88bc5d29426c2f0aeb7bd24f790cf429
Wayne <wayne.c.griffiths@gmail.com>
2020-06-05 11:11:06 +0800

    added MealPlan Model to db

--- routes/api/favourite.routes.js
+++ routes/api/favourite.routes.js
@@ -5,7 +5,7 @@ module.exports = app => {
 
     // this should be a boolean value yeah?
     app.post('/api/favourite',(req, res) =>{
-        const { recipeID } = req,body;
+        const { recipeID } = req.body;
         // add to favourites with db here
         // db.somequery idk plz halp
         //send back a response
@@ -13,7 +13,7 @@ module.exports = app => {
 
     // again? a boolean yeah?
     app.delete('/api/favourite',(req, res) =>{
-        const { recipeID } = req,body;
+        const { recipeID } = req.body;
         // delete from favourites with db here
         // db.somequery idk plz halp
         //send back a response
