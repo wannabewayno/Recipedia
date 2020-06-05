6bb510ebc295a08898840ace0f19e239468d3810
Wayne <wayne.c.griffiths@gmail.com>
2020-06-05 21:03:04 +0800

    added a callback function parameter to removeElement to trigger an action upon removing an element

---
+++ Public/assets/javascript/noScroll.js
@@ -0,0 +1,6 @@
+/**
+ * triggers the no-scroll class on the body of the html document.
+ */
+const noScroll = () => {
+    $('body').toggleClass('no-scroll');
+}
--- Public/assets/javascript/removeElement.js
+++ Public/assets/javascript/removeElement.js
@@ -3,16 +3,19 @@
  * Listens for clicks on a nominated delete selector e.g: a <button class="delete-btn">
  * On the specified container objects e.g: <div class="ingredients">
  * Removes the clicked on item whose parent has '.saved-data' class.  
- * @param {String} deleteSelector the class or id selector of the element that triggers a delete 
- * @param {String} selectorToDelete the class or id selector of the element to remove from the document 
- * @param {...String} targetContainers the class or id of the container that holds the '.saved-data' elements to delete
+ * @param {String} deleteSelector - the class or id selector of the element that triggers a delete 
+ * @param {String} selectorToDelete - the class or id selector of the element to remove from the document 
+ * @param {Array<string>} targetContainers - an array of class or id selectors of the container that holds the selectorToDelete elements to delete
+ * @param {Callback} callback - 
  */
-const removeElement = (deleteSelector,selectorToDelete,...targetContainers) => {
+const removeElement = (deleteSelector,selectorToDelete,targetContainers,callback) => {
 
     return $(targetContainers.join(', ')).on('click', deleteSelector ,function(event){
         event.stopPropagation();
-    
         const item = $(event.currentTarget).parents(selectorToDelete);
         item.remove();
+        
+        // name of a callback to execute.
+        callback();
     });
 }
