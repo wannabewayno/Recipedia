f0e8e442bc3b37c80c2e35875799afb3cd29dd73
RJC Desktop <reuben.crocker@gmail.com>
2020-06-06 10:54:55 +0800

    added mealplan seeder

---
+++ seeders/20200606025320-mealplan.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const faker = require('faker');
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+
+    return queryInterface.bulkInsert('favourites', [{
+      user_id: 1,
+      recipe_id: 1,
+      meal_slot: faker.lorem.words()
+    }], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:*/
+      return queryInterface.bulkDelete('favourites', null, {});
+  }
+};
