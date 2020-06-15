30ad71db8d47b76f81221962b3e9e166f2637b04
Wayne <wayne.c.griffiths@gmail.com>
2020-06-15 19:58:38 +0800

    UPDATE compression

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const db = require("./models");
 
 // Creating express app and configuring middleware needed for authentication
 const app = express();
-app.use(compression)
+app.use(compression())
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static("Public"));
